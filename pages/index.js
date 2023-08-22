2698abc0522e63d93ae4cf0addae0071fd3dd084
praise <essiakpevwepraise@gmail.com>
2023-08-22 07:06:07 -0700

    feat: separating the components

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,11 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import {data} from '../SpeakerData';
+import Speaker from './Speaker';
 
 const inter = Inter({ subsets: ['latin'] })
 
-function Session({session}) {
-  
-  const {title, room} = session;
 
-  return (
-    <span className="session w-100">
-                  {title}{" "}
-                  <strong>Room: {room.name}</strong>
-                </span>
-  )
-}
-
-function Sessions({ sessions }) {
-    return (
-      <div className="sessionBox card h-250">
-                <Session session={sessions[0]} />
-                
-         </div>
-    ); 
-}
 
 export default function Home() {
 
@@ -33,44 +15,9 @@ export default function Home() {
     <div className="container speakers-list">
       <div className="row">
         {data.map(function (speaker) {
-          const {
-            id,
-            bio,
-            first,
-            last,
-            favorite,
-            twitterHandle,
-            company,
-            sessions,
-          } = speaker;
-
+          
           return (
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12" key={id}>
-              <div className="card card-height p-4 mt-4" >
-                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-                  <img
-                    className="contain-fit"
-                    src={`/images/speaker-${id}.jpg`}
-                    width="300"
-                    alt={`${first} ${last}`}
-                    
-                  />
-                </div>
-                <div className="speaker-info">
-                  <div className="d-flex justify-content-between mb-3">
-                    <h3 className="text-truncate w-200">
-                      {first} {last}
-                    </h3>
-                  </div>
-                  <div>
-                    <p>
-                      {bio} {company} {twitterHandle} {favorite}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <Sessions sessions={sessions} />
-            </div>
+            <Speaker key={speaker.id} speaker={speaker}/>
           );
         })}
       </div>
